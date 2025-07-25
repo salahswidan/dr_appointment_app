615f2ee97b6c058994839ada983f869aba4ba46d
salah <salahswidan212@gmail.com>
2025-07-25 17:49:38 +0300

    feat: Implement dependency injection and API error handling
    - Created app_regex.dart for email and password validation regex patterns.
    - Introduced api_constants.dart
    - Created api_error_model.dart and its generated file for structured error handling.
    - Implemented api_result.dart and its generated file for handling API results.
    - Added api_service.dart and its generated file for Retrofit API service integration.
    - Introduced dio_factory.dart for configuring Dio HTTP client with interceptors.
    - Updated app_router.dart to utilize LoginCubit from dependency injection.
    - Created login_request_body.dart and its generated file for login request data model.
    - Developed login_response.dart and its generated file for login response data model.
    - Implemented login_repo.dart for login API interaction.
    - Created login_cubit.dart for managing login state with Bloc.
    - Added login_state.dart and its generated file for defining login states.
    - Updated main.dart to initialize dependency injection on app startup.

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,9 @@
+import 'package:dr_appointment_app/core/di/dependency_injection.dart';
 import 'package:dr_appointment_app/core/routing/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../features/login/logic/cubit/login_cubit.dart';
 import '../../features/login/ui/login_screen.dart';
 import '../../features/on_boarding/on_boarding_screen.dart';
 
@@ -9,22 +12,23 @@ class AppRouter {
     final arguments = settings.arguments;
     switch (settings.name) {
       case Routes.onBoardingScreen:
-        return MaterialPageRoute(
-          builder: (_) => const OnBoardingScreen(),
-        );
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       case Routes.loginScreen:
         return MaterialPageRoute(
-          builder: (_) => const LoginScreen(),
+          builder:(_) => BlocProvider(
+                create: (context) => getIt<LoginCubit>(),
+                child: const LoginScreen(),
+              ),
         );
       default:
         return MaterialPageRoute(
-          builder: (_) => Scaffold(
-            body: Center(
-              child: Text('No route defined for ${settings.name}'),
-            ),
-          ),
+          builder:
+              (_) => Scaffold(
+                body: Center(
+                  child: Text('No route defined for ${settings.name}'),
+                ),
+              ),
         );
     }
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:dr_appointment_app/doc_app.dart';
 import 'package:flutter/material.dart';
 
+import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 
 void main() {
+  setupGetIt();
   runApp( DocApp(
     appRouter: AppRouter(),
   ));
