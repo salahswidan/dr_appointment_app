091c871fa7b6285b64760a6b03dd6c23cc96a770
salah <salahswidan212@gmail.com>
2025-07-26 18:23:24 +0300

    Add Firebase configuration to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:dr_appointment_app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'core/routing/app_router.dart';
 
-void main() {
+void main()async {
   setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp( DocApp(
     appRouter: AppRouter(),
   ));
