d17d59043c931df5f35b536cdef958feaa9b1939
salah <salahswidan212@gmail.com>
2025-07-29 15:15:03 +0300

    Refactor SharedPrefHelper to use FlutterSecureStorage for secure token management

--- lib/core/helpers/shared_pref_helper.dart
+++ lib/core/helpers/shared_pref_helper.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SharedPrefHelper {
   // private constructor as I don't want to allow creating an instance of this class itself.
@@ -66,5 +67,26 @@ class SharedPrefHelper {
     debugPrint('SharedPrefHelper : getString with key : $key');
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     return sharedPreferences.getString(key) ?? '';
+  }
+
+  /// Saves a [value] with a [key] in the SharedPreferences.
+  static setSecuredString(String key, String value) async {
+    const flutterSecureStorage = FlutterSecureStorage();
+    debugPrint('FlutterSecureStorage : setSecuredString with key : $key and value : $value');
+    await flutterSecureStorage.write(key: key, value: value);
+  }
+  /// Gets a secured string value from FlutterSecureStorage with given [key].
+  static  getSecuredString(String key) async {
+    const flutterSecureStorage = FlutterSecureStorage();
+    debugPrint('FlutterSecureStorage : getSecuredString with key : $key');
+    return await flutterSecureStorage.read(key: key) ?? '';
+  }
+
+  
+  /// Removes all keys and values in the FlutterSecureStorage
+  static clearAllSecuredData() async {
+    debugPrint('SharedPrefHelper : all data has been cleared');
+    const flutterSecureStorage = FlutterSecureStorage();
+    await flutterSecureStorage.deleteAll();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ void main() async {
 }
 
 checkIfLoggedInUser() async {
-  String? userToken = await SharedPrefHelper.getString(
+  String? userToken = await SharedPrefHelper.getSecuredString(
     SharedPrefKeys.userToken,
   );
   if (!userToken.isNullOrEmpty()) {
